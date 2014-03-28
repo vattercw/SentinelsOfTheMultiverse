b785f9831951bffbd5055c3aba9fe6ca57e96e50
Corey Vatter <vattercw@rose-hulman.edu>
2014-03-28 00:52:16 -0400

    Never gonna say goodbye

---
+++ SentinelsOfTheMultiverse/SentinelsOfTheMultiverse/Tests/VillainTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SentinelsOfTheMultiverse.Data;
+using SentinelsOfTheMultiverse.Data.Villains;
+
+namespace SentinelsOfTheMultiverse.Tests
+{
+    [TestFixture]
+    class VillainTest
+    {
+        [Test, RequiresSTA]
+        public void TestVillainInitalization()
+        {
+            Villain villainTest = new BaronBlade();
+            Assert.AreNotEqual(null, villainTest);
+            //Assert.AreEqual("Baron Blade", villainTest.getCharacterName());
+        }
+
+    }
+}
