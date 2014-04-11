de91e1be9dc018309e0dd1ab571174c1df1c2243
Corey Vatter <vattercw@rose-hulman.edu>
2014-04-10 23:28:10 -0400

    shhhh jearles

--- SentinelsOfTheMultiverse/SentinelsOfTheMultiverse/Data/Minions/BaronBlade/PoweredRemoteTurret.cs
+++ SentinelsOfTheMultiverse/SentinelsOfTheMultiverse/Data/Minions/BaronBlade/PoweredRemoteTurret.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SentinelsOfTheMultiverse.Data.Effects;
 
 namespace SentinelsOfTheMultiverse.Data.Minions
 {
@@ -18,7 +19,21 @@ namespace SentinelsOfTheMultiverse.Data.Minions
 
         public override void executeEffect()
         {
-            throw new NotImplementedException();
+            int numTurret = 0;
+            for (int i = 0; i < GameEngine.getVillain().getEndPhaseMinions().Count; i++)
+            {
+                if (GameEngine.getVillain().getEndPhaseMinions()[i] is PoweredRemoteTurret)
+                {
+                    numTurret++;
+                }
+            }
+
+            int extraDamage = numTurret - 1;
+
+            for (int i = 0; i < GameEngine.getHeroes().Count; i++)
+            {
+                DamageEffects.DealDamage(GameEngine.getHeroes(), null, null, 2, DamageEffects.DamageType.Projectile);
+            }
         }
     }
 }
