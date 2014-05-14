987711892bafa31c15a697ff88acf642584a761e
Corey Vatter <vattercw@rose-hulman.edu>
2014-05-13 22:44:26 -0400

    Motivational charge more like botivational barge!

--- SentinelsOfTheMultiverse/SentinelsOfTheMultiverse/Data/Heroes/Legacy.cs
+++ SentinelsOfTheMultiverse/SentinelsOfTheMultiverse/Data/Heroes/Legacy.cs
@@ -38,6 +38,7 @@ namespace SentinelsOfTheMultiverse.Data.Heroes
             this.immuneToEnvironment = true;
         }
 
+
         public void FlyingSmash(Card card)
         {
         }
@@ -108,6 +109,56 @@ namespace SentinelsOfTheMultiverse.Data.Heroes
 
         public void MotivationalCharge(Card card)
         {
+            card.cardPower = new Card.Power(PunishTheWeakPower);
+        }
+
+        void PunishTheWeakPower(Card card, object[] args)
+        {
+            var target = GameBoard.cardClickedArray;
+            if (target.Count > 1)
+            {
+                MessageBox.Show("Select only one target to deal damage to. \n No select target default's to the Villain.");
+                Hero currentPlayer = (Hero)GameEngine.getCurrentPlayer();
+                cardsOnField.Remove(card);
+                currentPlayer.hand.Add(card);
+                GameEngine.playerPlayedCard = false;
+            }
+            else if (target.Count == 1)
+            {
+                var villainMinions = GameEngine.getVillain().getMinions();
+                var environMinions = GameEngine.getEnvironment().getMinions();
+                
+                foreach (Minion min in villainMinions)
+                {
+                    if (min.minionName == target[0].Name)
+                    {
+                        List<Minion> mins = new List<Minion>();
+                        mins.Add(min);
+                        DamageEffects.DealDamage(null, null, mins, 2, DamageEffects.DamageType.Melee);
+                        break;
+                    }
+                }
+
+                foreach (Minion min in environMinions)
+                {
+                    if (min.minionName == target[0].Name)
+                    {
+                        List<Minion> mins = new List<Minion>();
+                        mins.Add(min);
+                        DamageEffects.DealDamage(null, null, mins, 2, DamageEffects.DamageType.Melee);
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                DamageEffects.DealDamage(null, GameEngine.getVillain(), null, 2, DamageEffects.DamageType.Melee);
+            }
+
+            foreach (Hero hero in GameEngine.getHeroes())
+            {
+                HealEffects.healHero(hero, 1);
+            }
         }
 
         public void InspiringPresence(Card card)
@@ -209,5 +260,7 @@ namespace SentinelsOfTheMultiverse.Data.Heroes
         {
             throw new NotImplementedException();
         }
+
+        
     }
 }
